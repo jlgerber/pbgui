1503fd001e3f873092e9ada7ef61e04d2c208a22
jgerber <jlgerber@gmail.com>
2019-12-11 18:01:29 -0800

    rename items

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,11 @@ const COLUMNS: i32 = 7;
 struct Form<'a> {
     _db: &'a mut PackratDb,
     _widget: CppBox<QWidget>,
-    _button: MutPtr<QPushButton>,
-    _line_edit: MutPtr<QLineEdit>,
-    _table_widget: MutPtr<QTableWidget>,
+    _query_button: MutPtr<QPushButton>,
+    _pkg_line_edit: MutPtr<QLineEdit>,
+    _vpin_table: MutPtr<QTableWidget>,
     _grpbox: MutPtr<QGroupBox>,
-    _twh: Vec<MutPtr<QTableWidgetItem>>,
+    _pkg_header_items: Vec<MutPtr<QTableWidgetItem>>,
     _qspacer: MutPtr<QSpacerItem>,
     button_clicked: Slot<'a>,
 }
@@ -90,16 +90,17 @@ impl<'a> Form<'a> {
     }
 
     unsafe fn setup_headers(
-        mut tw: MutPtr<QTableWidget>,
+        mut vpin_tablewidget: MutPtr<QTableWidget>,
         headers: Vec<&'static str>,
     ) -> (MutPtr<QTableWidget>, Vec<MutPtr<QTableWidgetItem>>) {
-        let mut qtwi_vec = Vec::new();
+        let mut pkg_header_items_vec = Vec::new();
         for (cnt, val) in headers.into_iter().enumerate() {
-            let mut qtwi = QTableWidgetItem::from_q_string(&QString::from_std_str(val));
-            tw.set_horizontal_header_item(cnt as i32, &mut qtwi);
-            qtwi_vec.push(qtwi.into_ptr());
+            let mut vpin_table_widget_item =
+                QTableWidgetItem::from_q_string(&QString::from_std_str(val));
+            vpin_tablewidget.set_horizontal_header_item(cnt as i32, &mut vpin_table_widget_item);
+            pkg_header_items_vec.push(vpin_table_widget_item.into_ptr());
         }
-        (tw, qtwi_vec)
+        (vpin_tablewidget, pkg_header_items_vec)
     }
 
     fn new(mut db: &'a mut PackratDb) -> Form<'a> {
@@ -127,27 +128,30 @@ impl<'a> Form<'a> {
 
             layout.add_widget(&mut line_edit);
             let line_edit = line_edit.into_ptr();
-            let mut tw = QTableWidget::new_2a(0, COLUMNS);
-            let mut tablemod = tw.model();
-            tw.vertical_header().hide();
-            tw.set_selection_behavior(SelectionBehavior::SelectRows);
-            tw.set_edit_triggers(QFlags::from(EditTrigger::NoEditTriggers));
-            tw.set_selection_mode(SelectionMode::SingleSelection);
-            tw.horizontal_header().set_stretch_last_section(true);
-            tw.horizontal_header()
+            let mut vpin_tablewidget = QTableWidget::new_2a(0, COLUMNS);
+            //let mut tablemod = vpin_tablewidget.model();
+            vpin_tablewidget.vertical_header().hide();
+            vpin_tablewidget.set_selection_behavior(SelectionBehavior::SelectRows);
+            vpin_tablewidget.set_edit_triggers(QFlags::from(EditTrigger::NoEditTriggers));
+            vpin_tablewidget.set_selection_mode(SelectionMode::SingleSelection);
+            vpin_tablewidget
+                .horizontal_header()
+                .set_stretch_last_section(true);
+            vpin_tablewidget
+                .horizontal_header()
                 .set_section_resize_mode_1a(ResizeMode::Stretch);
-            tw.set_show_grid(false);
-            tw.set_alternating_row_colors(true);
-            tw.set_style_sheet(&QString::from_std_str(
+            vpin_tablewidget.set_show_grid(false);
+            vpin_tablewidget.set_alternating_row_colors(true);
+            vpin_tablewidget.set_style_sheet(&QString::from_std_str(
                 "alternate-background-color: rgb(50,50,50);color: rgb(200,200,200);background-color: rgb(40,40,40);",
             ));
-            tw.horizontal_header()
+            vpin_tablewidget.horizontal_header()
                 .set_style_sheet(&QString::from_std_str(
                     "background-color: rgb(80,80,80); color:white; border:none;outline:none;border-left: 0px; border-right: 0px;",
                 ));
 
-            let (tw, qtwi_vec) = Self::setup_headers(
-                tw.into_ptr(),
+            let (vpin_tablewidget, pkg_header_items_vec) = Self::setup_headers(
+                vpin_tablewidget.into_ptr(),
                 vec![
                     "Id",
                     "Distribution",
@@ -164,8 +168,8 @@ impl<'a> Form<'a> {
             hlayout.add_widget(&mut button);
             let button = button.into_ptr();
 
-            layout.add_widget(tw);
-            let mut tw_ptr = tw.clone();
+            layout.add_widget(vpin_tablewidget);
+            let mut vpin_tablewidget_ptr = vpin_tablewidget.clone();
             // let mut button = QPushButton::from_q_string(&QString::from_std_str("Query"));
 
             // layout.add_widget(&mut button);
@@ -179,38 +183,39 @@ impl<'a> Form<'a> {
                     let line_edit_txt = line_edit.text().to_std_string();
 
                     let showtxt = level_ptr.current_text();
-                    let mut qtwi = QTableWidgetItem::new();
-                    qtwi.set_text(&showtxt);
+                    let mut vpin_table_widget_item = QTableWidgetItem::new();
+                    vpin_table_widget_item.set_text(&showtxt);
                     let showtxt = showtxt.to_std_string();
-                    tw_ptr.set_item(0, 1, qtwi.into_ptr());
+                    vpin_tablewidget_ptr.set_item(0, 1, vpin_table_widget_item.into_ptr());
 
                     let roletxt = role_ptr.current_text();
-                    let mut qtwi = QTableWidgetItem::new();
-                    qtwi.set_text(&roletxt);
+                    let mut vpin_table_widget_item = QTableWidgetItem::new();
+                    vpin_table_widget_item.set_text(&roletxt);
                     let roletxt = roletxt.to_std_string();
-                    tw_ptr.set_item(0, 2, qtwi.into_ptr());
+                    vpin_tablewidget_ptr.set_item(0, 2, vpin_table_widget_item.into_ptr());
 
                     let platformtxt = platform_ptr.current_text();
-                    let mut qtwi = QTableWidgetItem::new();
-                    qtwi.set_text(&platformtxt);
+                    let mut vpin_table_widget_item = QTableWidgetItem::new();
+                    vpin_table_widget_item.set_text(&platformtxt);
                     let platformtxt = platformtxt.to_std_string();
-                    tw_ptr.set_item(0, 3, qtwi.into_ptr());
+                    vpin_tablewidget_ptr.set_item(0, 3, vpin_table_widget_item.into_ptr());
 
                     let sitetxt = site_ptr.current_text();
-                    let mut qtwi = QTableWidgetItem::new();
-                    qtwi.set_text(&sitetxt);
+                    let mut vpin_table_widget_item = QTableWidgetItem::new();
+                    vpin_table_widget_item.set_text(&sitetxt);
                     let sitetxt = sitetxt.to_std_string();
-                    tw_ptr.set_item(0, 4, qtwi.into_ptr());
+                    vpin_tablewidget_ptr.set_item(0, 4, vpin_table_widget_item.into_ptr());
                     // for now
-                    let mut client = Client::connect(
+                    let client = Client::connect(
                         "host=127.0.0.1 user=postgres dbname=packrat password=example port=5432",
                         NoTls,
                     )
                     .unwrap();
-                    let mut pbdb = PackratDb::new(client);
-                    let mut pb = pbdb.find_all_versionpins();
+                    let mut packratdb = PackratDb::new(client);
+                    let mut vpin_finder = packratdb.find_all_versionpins();
 
-                    pb.level(showtxt.as_str())
+                    vpin_finder
+                        .level(showtxt.as_str())
                         .role(roletxt.as_str())
                         .platform(platformtxt.as_str())
                         .site(sitetxt.as_str())
@@ -218,11 +223,11 @@ impl<'a> Form<'a> {
                             LtreeSearchMode::from_str(dirtxt.to_std_string().as_str()).unwrap(),
                         );
                     let filter_package = if line_edit_txt != "" { true } else { false };
-                    let results = pb.query().unwrap();
+                    let results = vpin_finder.query().unwrap();
                     let mut cnt = 0;
-                    tw_ptr.set_sorting_enabled(false);
-                    tw_ptr.set_row_count(0);
-                    tw_ptr.set_row_count(results.len() as i32);
+                    vpin_tablewidget_ptr.set_sorting_enabled(false);
+                    vpin_tablewidget_ptr.set_row_count(0);
+                    vpin_tablewidget_ptr.set_row_count(results.len() as i32);
                     for result in results {
                         if filter_package && line_edit_txt != "" {
                             if !result
@@ -234,57 +239,57 @@ impl<'a> Form<'a> {
                             }
                         }
                         // ID TODO: get it sorting correctly... (data)
-                        let mut qtwi = QTableWidgetItem::new();
-                        qtwi.set_text(&QString::from_std_str(
+                        let mut vpin_table_widget_item = QTableWidgetItem::new();
+                        vpin_table_widget_item.set_text(&QString::from_std_str(
                             result.versionpin_id.to_string().as_str(),
                         ));
-                        tw_ptr.set_item(cnt, 0, qtwi.into_ptr());
+                        vpin_tablewidget_ptr.set_item(cnt, 0, vpin_table_widget_item.into_ptr());
                         // DISTRIBUTION
-                        let mut qtwi = QTableWidgetItem::new();
-                        qtwi.set_text(&QString::from_std_str(
+                        let mut vpin_table_widget_item = QTableWidgetItem::new();
+                        vpin_table_widget_item.set_text(&QString::from_std_str(
                             result.distribution.to_string().as_str(),
                         ));
-                        tw_ptr.set_item(cnt, 1, qtwi.into_ptr());
+                        vpin_tablewidget_ptr.set_item(cnt, 1, vpin_table_widget_item.into_ptr());
                         // LEVEL
-                        let mut qtwi = QTableWidgetItem::new();
-                        qtwi.set_text(&QString::from_std_str(
+                        let mut vpin_table_widget_item = QTableWidgetItem::new();
+                        vpin_table_widget_item.set_text(&QString::from_std_str(
                             result.coords.level.to_string().as_str(),
                         ));
-                        tw_ptr.set_item(cnt, 2, qtwi.into_ptr());
+                        vpin_tablewidget_ptr.set_item(cnt, 2, vpin_table_widget_item.into_ptr());
                         // ROLE
-                        let mut qtwi = QTableWidgetItem::new();
-                        qtwi.set_text(&QString::from_std_str(
+                        let mut vpin_table_widget_item = QTableWidgetItem::new();
+                        vpin_table_widget_item.set_text(&QString::from_std_str(
                             result.coords.role.to_string().as_str(),
                         ));
-                        tw_ptr.set_item(cnt, 3, qtwi.into_ptr());
+                        vpin_tablewidget_ptr.set_item(cnt, 3, vpin_table_widget_item.into_ptr());
                         // PLATFORM
-                        let mut qtwi = QTableWidgetItem::new();
-                        qtwi.set_text(&QString::from_std_str(
+                        let mut vpin_table_widget_item = QTableWidgetItem::new();
+                        vpin_table_widget_item.set_text(&QString::from_std_str(
                             result.coords.platform.to_string().as_str(),
                         ));
-                        tw_ptr.set_item(cnt, 4, qtwi.into_ptr());
+                        vpin_tablewidget_ptr.set_item(cnt, 4, vpin_table_widget_item.into_ptr());
                         // SITE
-                        let mut qtwi = QTableWidgetItem::new();
-                        qtwi.set_text(&QString::from_std_str(
+                        let mut vpin_table_widget_item = QTableWidgetItem::new();
+                        vpin_table_widget_item.set_text(&QString::from_std_str(
                             result.coords.site.to_string().as_str(),
                         ));
-                        tw_ptr.set_item(cnt, 5, qtwi.into_ptr());
+                        vpin_tablewidget_ptr.set_item(cnt, 5, vpin_table_widget_item.into_ptr());
                         // WITHS
-                        let mut qtwi = QTableWidgetItem::new();
-                        qtwi.set_text(&QString::from_std_str(
+                        let mut vpin_table_widget_item = QTableWidgetItem::new();
+                        vpin_table_widget_item.set_text(&QString::from_std_str(
                             result.withs.map_or(0, |x| x.len()).to_string().as_str(),
                         ));
-                        tw_ptr.set_item(cnt, 6, qtwi.into_ptr());
+                        vpin_tablewidget_ptr.set_item(cnt, 6, vpin_table_widget_item.into_ptr());
                         cnt += 1;
                     }
-                    tw_ptr.set_sorting_enabled(true);
+                    vpin_tablewidget_ptr.set_sorting_enabled(true);
                 }),
                 _db: db,
                 _widget: widget,
-                _table_widget: tw,
-                _twh: qtwi_vec,
-                _button: button,
-                _line_edit: line_edit,
+                _vpin_table: vpin_tablewidget,
+                _pkg_header_items: pkg_header_items_vec,
+                _query_button: button,
+                _pkg_line_edit: line_edit,
                 _grpbox: grpbox,
                 _qspacer: qspacer.into_ptr(),
                 //_boxes: boxes,
@@ -297,14 +302,13 @@ impl<'a> Form<'a> {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = Client::connect(
+    let client = Client::connect(
         "host=127.0.0.1 user=postgres dbname=packrat password=example port=5432",
         NoTls,
     )?;
-    let mut pb = PackratDb::new(client);
+    let mut vpin_finder = PackratDb::new(client);
     QApplication::init(|_| unsafe {
-        let _form = Form::new(&mut pb);
+        let _form = Form::new(&mut vpin_finder);
         QApplication::exec()
     });
-    Ok(())
 }
