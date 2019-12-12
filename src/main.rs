4ee3b8072b8ac507486a625641180ac207e1e908
jgerber <jlgerber@gmail.com>
2019-12-11 17:53:27 -0800

    working

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 #![windows_subsystem = "windows"]
 use packybara::packrat::{Client, NoTls, PackratDb};
 use packybara::LtreeSearchMode;
+use qt_core::QFlags;
 use qt_widgets::{
     cpp_core::{CppBox, MutPtr},
+    q_abstract_item_view::{EditTrigger, SelectionBehavior, SelectionMode},
     q_header_view::ResizeMode,
+    q_size_policy::Policy,
     qt_core::QString,
     qt_core::Slot,
-    QApplication, QComboBox, QGroupBox, QHBoxLayout, QLineEdit, QPushButton, QTableWidget,
-    QTableWidgetItem, QVBoxLayout, QWidget,
+    QApplication, QComboBox, QGroupBox, QHBoxLayout, QLineEdit, QPushButton, QSpacerItem,
+    QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget,
 };
-use std::str::FromStr;
 
+use std::str::FromStr;
+const COLUMNS: i32 = 7;
 struct Form<'a> {
     _db: &'a mut PackratDb,
     _widget: CppBox<QWidget>,
@@ -19,6 +23,7 @@ struct Form<'a> {
     _table_widget: MutPtr<QTableWidget>,
     _grpbox: MutPtr<QGroupBox>,
     _twh: Vec<MutPtr<QTableWidgetItem>>,
+    _qspacer: MutPtr<QSpacerItem>,
     button_clicked: Slot<'a>,
 }
 
@@ -32,13 +37,17 @@ impl<'a> Form<'a> {
         CppBox<QComboBox>,
         CppBox<QComboBox>,
         CppBox<QComboBox>,
+        CppBox<QSpacerItem>,
     ) {
         //results
         let mut lcb = QComboBox::new_0a();
         let results = db.find_all_levels().query().unwrap();
+        lcb.add_item_q_string(&QString::from_std_str("facility"));
         for r in results {
             let level_str = r.level.as_str();
-            lcb.add_item_q_string(&QString::from_std_str(level_str));
+            if level_str != "facility" {
+                lcb.add_item_q_string(&QString::from_std_str(level_str));
+            }
         }
         layout.add_widget(&mut lcb);
 
@@ -74,7 +83,10 @@ impl<'a> Form<'a> {
             direction.add_item_q_string(&QString::from_std_str(r));
         }
         layout.add_widget(&mut direction);
-        (lcb, rcb, pcb, scb, direction)
+
+        let mut qspacer = QSpacerItem::new_3a(30, 10, Policy::Expanding);
+        layout.add_item(&mut qspacer);
+        (lcb, rcb, pcb, scb, direction, qspacer)
     }
 
     unsafe fn setup_headers(
@@ -102,7 +114,7 @@ impl<'a> Form<'a> {
             let mut grpbox = grpbox.into_ptr();
             // layout for combo boxes
             let mut hlayout = QHBoxLayout::new_0a().into_ptr();
-            let (level, role, platform, site, direction) =
+            let (level, role, platform, site, direction, qspacer) =
                 Self::combo_boxes(&mut db, hlayout.clone());
             let level_ptr = level.into_ptr();
             let role_ptr = role.into_ptr();
@@ -115,15 +127,19 @@ impl<'a> Form<'a> {
 
             layout.add_widget(&mut line_edit);
             let line_edit = line_edit.into_ptr();
-            let mut tw = QTableWidget::new_2a(5, 5);
+            let mut tw = QTableWidget::new_2a(0, COLUMNS);
+            let mut tablemod = tw.model();
             tw.vertical_header().hide();
+            tw.set_selection_behavior(SelectionBehavior::SelectRows);
+            tw.set_edit_triggers(QFlags::from(EditTrigger::NoEditTriggers));
+            tw.set_selection_mode(SelectionMode::SingleSelection);
             tw.horizontal_header().set_stretch_last_section(true);
             tw.horizontal_header()
                 .set_section_resize_mode_1a(ResizeMode::Stretch);
             tw.set_show_grid(false);
             tw.set_alternating_row_colors(true);
             tw.set_style_sheet(&QString::from_std_str(
-                "alternate-background-color: rgb(70,70,70);color: rgb(200,200,200);background-color: rgb(60,60,60);",
+                "alternate-background-color: rgb(50,50,50);color: rgb(200,200,200);background-color: rgb(40,40,40);",
             ));
             tw.horizontal_header()
                 .set_style_sheet(&QString::from_std_str(
@@ -132,10 +148,19 @@ impl<'a> Form<'a> {
 
             let (tw, qtwi_vec) = Self::setup_headers(
                 tw.into_ptr(),
-                vec!["Distribution", "Level", "Role", "Platform", "Site"],
+                vec![
+                    "Id",
+                    "Distribution",
+                    "Level",
+                    "Role",
+                    "Platform",
+                    "Site",
+                    "Withs",
+                ],
             );
             let mut button = QPushButton::from_q_string(&QString::from_std_str("Query"));
-
+            button.set_minimum_width(70);
+            button.set_maximum_width(70);
             hlayout.add_widget(&mut button);
             let button = button.into_ptr();
 
@@ -151,25 +176,30 @@ impl<'a> Form<'a> {
             let form = Form {
                 button_clicked: Slot::new(move || {
                     let dirtxt = dir_ptr.current_text();
+                    let line_edit_txt = line_edit.text().to_std_string();
 
                     let showtxt = level_ptr.current_text();
                     let mut qtwi = QTableWidgetItem::new();
                     qtwi.set_text(&showtxt);
+                    let showtxt = showtxt.to_std_string();
                     tw_ptr.set_item(0, 1, qtwi.into_ptr());
 
                     let roletxt = role_ptr.current_text();
                     let mut qtwi = QTableWidgetItem::new();
                     qtwi.set_text(&roletxt);
+                    let roletxt = roletxt.to_std_string();
                     tw_ptr.set_item(0, 2, qtwi.into_ptr());
 
                     let platformtxt = platform_ptr.current_text();
                     let mut qtwi = QTableWidgetItem::new();
                     qtwi.set_text(&platformtxt);
+                    let platformtxt = platformtxt.to_std_string();
                     tw_ptr.set_item(0, 3, qtwi.into_ptr());
 
                     let sitetxt = site_ptr.current_text();
                     let mut qtwi = QTableWidgetItem::new();
                     qtwi.set_text(&sitetxt);
+                    let sitetxt = sitetxt.to_std_string();
                     tw_ptr.set_item(0, 4, qtwi.into_ptr());
                     // for now
                     let mut client = Client::connect(
@@ -177,54 +207,77 @@ impl<'a> Form<'a> {
                         NoTls,
                     )
                     .unwrap();
-                    let mut pb = PackratDb::new(client);
-                    let results = pb
-                        .find_all_versionpins()
-                        .level(showtxt.to_std_string().as_str())
-                        .role(roletxt.to_std_string().as_str())
-                        .platform(platformtxt.to_std_string().as_str())
-                        .site(sitetxt.to_std_string().as_str())
+                    let mut pbdb = PackratDb::new(client);
+                    let mut pb = pbdb.find_all_versionpins();
+
+                    pb.level(showtxt.as_str())
+                        .role(roletxt.as_str())
+                        .platform(platformtxt.as_str())
+                        .site(sitetxt.as_str())
                         .search_mode(
                             LtreeSearchMode::from_str(dirtxt.to_std_string().as_str()).unwrap(),
-                        )
-                        .query()
-                        .unwrap();
+                        );
+                    let filter_package = if line_edit_txt != "" { true } else { false };
+                    let results = pb.query().unwrap();
                     let mut cnt = 0;
+                    tw_ptr.set_sorting_enabled(false);
                     tw_ptr.set_row_count(0);
                     tw_ptr.set_row_count(results.len() as i32);
                     for result in results {
+                        if filter_package && line_edit_txt != "" {
+                            if !result
+                                .distribution
+                                .package()
+                                .contains(line_edit_txt.as_str())
+                            {
+                                continue;
+                            }
+                        }
+                        // ID TODO: get it sorting correctly... (data)
+                        let mut qtwi = QTableWidgetItem::new();
+                        qtwi.set_text(&QString::from_std_str(
+                            result.versionpin_id.to_string().as_str(),
+                        ));
+                        tw_ptr.set_item(cnt, 0, qtwi.into_ptr());
+                        // DISTRIBUTION
                         let mut qtwi = QTableWidgetItem::new();
                         qtwi.set_text(&QString::from_std_str(
                             result.distribution.to_string().as_str(),
                         ));
-                        tw_ptr.set_item(cnt, 0, qtwi.into_ptr());
-
+                        tw_ptr.set_item(cnt, 1, qtwi.into_ptr());
+                        // LEVEL
                         let mut qtwi = QTableWidgetItem::new();
                         qtwi.set_text(&QString::from_std_str(
                             result.coords.level.to_string().as_str(),
                         ));
-                        tw_ptr.set_item(cnt, 1, qtwi.into_ptr());
-
+                        tw_ptr.set_item(cnt, 2, qtwi.into_ptr());
+                        // ROLE
                         let mut qtwi = QTableWidgetItem::new();
                         qtwi.set_text(&QString::from_std_str(
                             result.coords.role.to_string().as_str(),
                         ));
-                        tw_ptr.set_item(cnt, 2, qtwi.into_ptr());
-
+                        tw_ptr.set_item(cnt, 3, qtwi.into_ptr());
+                        // PLATFORM
                         let mut qtwi = QTableWidgetItem::new();
                         qtwi.set_text(&QString::from_std_str(
                             result.coords.platform.to_string().as_str(),
                         ));
-                        tw_ptr.set_item(cnt, 3, qtwi.into_ptr());
-
+                        tw_ptr.set_item(cnt, 4, qtwi.into_ptr());
+                        // SITE
                         let mut qtwi = QTableWidgetItem::new();
                         qtwi.set_text(&QString::from_std_str(
                             result.coords.site.to_string().as_str(),
                         ));
-                        tw_ptr.set_item(cnt, 4, qtwi.into_ptr());
-
+                        tw_ptr.set_item(cnt, 5, qtwi.into_ptr());
+                        // WITHS
+                        let mut qtwi = QTableWidgetItem::new();
+                        qtwi.set_text(&QString::from_std_str(
+                            result.withs.map_or(0, |x| x.len()).to_string().as_str(),
+                        ));
+                        tw_ptr.set_item(cnt, 6, qtwi.into_ptr());
                         cnt += 1;
                     }
+                    tw_ptr.set_sorting_enabled(true);
                 }),
                 _db: db,
                 _widget: widget,
@@ -233,6 +286,7 @@ impl<'a> Form<'a> {
                 _button: button,
                 _line_edit: line_edit,
                 _grpbox: grpbox,
+                _qspacer: qspacer.into_ptr(),
                 //_boxes: boxes,
             };
             button.clicked().connect(&form.button_clicked);
