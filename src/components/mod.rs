074449fa44ee609374deb4c1f1b79bb992eb2933
jgerber <jlgerber@gmail.com>
2019-12-17 08:05:06 -0800

    moves all components under a components mod

---
+++ src/components/mod.rs
@@ -0,0 +1,4 @@
+pub mod search_bar;
+pub mod table_headers;
+pub mod versionpin_changes_table;
+pub mod versionpin_table;
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 pub mod constants;
 pub mod slot_functions;
-pub mod table_headers;
 pub mod utility;
-pub mod versionpin_changes_table;
-pub mod versionpin_table;
+// needed so that qt wont segfault
+pub mod components;
+pub use components::{
+    search_bar::{combo_boxes, create_query_button},
+    table_headers, versionpin_changes_table, versionpin_table,
+};
 pub use slot_functions::{choose_distribution, save_versionpin_changes, update_versionpin_table};
-pub mod search_bar;
-pub use search_bar::{combo_boxes, create_query_button};
