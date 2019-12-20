5e30e2c23d1e79fbae1d908f350e50801e7dc52e
jgerber <jlgerber@gmail.com>
2019-12-19 20:36:14 -0800

    adds log init to main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 #![windows_subsystem = "windows"]
+use env_logger;
+use log;
 use packybara::packrat::PackratDb;
 use pbgui::{parent_form, ClientProxy};
 use qt_widgets::QApplication;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
     let client = ClientProxy::connect()?;
     let mut vpin_finder = PackratDb::new(client);
     QApplication::init(|_| unsafe {
