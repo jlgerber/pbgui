7a4bc728e05db35de9ee8929c13fc66832766237
jgerber <jlgerber@gmail.com>
2019-12-19 21:58:50 -0800

    adds logging

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
 #![windows_subsystem = "windows"]
 use env_logger;
+use env_logger::Env;
 use log;
 use packybara::packrat::PackratDb;
 use pbgui::{parent_form, ClientProxy};
 use qt_widgets::QApplication;
+use std::env;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug, PartialEq)]
+pub struct PbGui {
+    /// Set the log level. This may target one or more
+    /// specific modules or be general.
+    /// (levels: trace, debug, info, warn, error)
+    #[structopt(long)]
+    pub loglevel: Option<String>,
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    env_logger::init();
+    let opt = PbGui::from_args();
+    if let PbGui {
+        loglevel: Some(ref level),
+        ..
+    } = opt
+    {
+        env::set_var("RUST_LOG", level);
+    }
+    env_logger::from_env(Env::default().default_filter_or("warn")).init();
+
     let client = ClientProxy::connect()?;
     let mut vpin_finder = PackratDb::new(client);
     QApplication::init(|_| unsafe {
