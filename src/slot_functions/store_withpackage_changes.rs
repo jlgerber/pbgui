32a6a82a2e9329465667953080151ddfd3c9f437
jgerber <jlgerber@gmail.com>
2019-12-27 14:29:27 -0800

    switces to use VersionPinRow<CppBox<QString>> from VersionPinRow<String> in store_withpackage_changes.rs

--- src/slot_functions/store_withpackage_changes.rs
+++ src/slot_functions/store_withpackage_changes.rs
@@ -1,14 +1,17 @@
 use crate::cache::PinChangesCache;
 use crate::change_type::ChangeType;
-use crate::components::versionpin_table::VersionPinRow;
-use crate::constants::*;
-use qt_widgets::{cpp_core::MutPtr, QListWidget, QTableWidget};
+use crate::versionpin_row::{VersionPinRow, VersionPinRowTrait};
+use qt_core::QString;
+use qt_widgets::{
+    cpp_core::{CppBox, MutPtr},
+    QListWidget, QTableWidget,
+};
 use std::rc::Rc;
 /// add the withpackage change to the listx`
 pub fn store_withpackage_changes(
     withpackage_list: MutPtr<QListWidget>,
     versionpin_table: MutPtr<QTableWidget>,
-    changes_table: &mut MutPtr<QTableWidget>,
+    _changes_table: &mut MutPtr<QTableWidget>,
     cache: Rc<PinChangesCache>,
 ) {
     unsafe {
@@ -22,14 +25,14 @@ pub fn store_withpackage_changes(
         let selection_model = versionpin_table.selection_model();
         if selection_model.has_selection() {
             let row = selection_model.selected_rows_0a().first().row();
-            let change_type = ChangeType::Withs;
-            let table_row = VersionPinRow::from_table_at_row(&versionpin_table, row);
+            let _change_type = ChangeType::Withs;
+            let table_row =
+                VersionPinRow::<CppBox<QString>>::from_table_at_row(&versionpin_table, row);
             if table_row.is_none() {
                 log::warn!("Tablerow {} is None", row);
                 return;
             }
             let table_row = table_row.unwrap();
-            //let vpin_id = versionpin_table.item(row, COL_ID).data(2).to_int_0a();
             println!("table row: {:#?}", table_row);
             println!("New Withs:\n{:#?}", &items);
             cache.cache_withs(table_row.id, items);
