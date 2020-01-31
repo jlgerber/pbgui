f1f79f6eb3da7767bb920a7dd7aeebd40bde294a
jlgerber <jlgerber@gmail.com>
2020-01-30 22:54:33 -0800

    adds impl for metadata logger vis controls

--- pbgui-logger/src/inner_log_win.rs
+++ pbgui-logger/src/inner_log_win.rs
@@ -6,8 +6,9 @@ use qt_widgets::{
     cpp_core::{CastInto, CppBox, MutPtr, Ref as QRef},
     q_abstract_item_view::SelectionBehavior,
     q_header_view::ResizeMode,
-    QFrame, QGroupBox, QLabel, QPushButton, QTableView, QWidget,
+    QCheckBox, QFrame, QGroupBox, QHBoxLayout, QPushButton, QTableView, QWidget,
 };
+
 use rustqt_utils::{create_hlayout, create_vlayout, qs, set_stylesheet_from_str};
 
 const STYLE_STR: &'static str = include_str!("../resources/pbgui_logger.qss");
@@ -48,16 +49,74 @@ impl<'a> LogData<'a> {
     }
 }
 
+/// Configuration for the log level in the log ui's controls, dictating
+/// the state of the checkboxes for log level
+pub struct LogLevelCtrlsConfig {
+    pub trace: bool,
+    pub debug: bool,
+    pub info: bool,
+    pub warn: bool,
+    pub error: bool,
+}
+
+impl Default for LogLevelCtrlsConfig {
+    fn default() -> Self {
+        Self {
+            trace: false,
+            debug: true,
+            info: true,
+            warn: true,
+            error: true,
+        }
+    }
+}
+
+/// Configuration for the log metadata in the log ui's controls, dictating
+/// the state of the checkboxes for log metadata
+pub struct LogMetadataCtrlsConfig {
+    pub level: bool,
+    pub datetime: bool,
+    pub target: bool,
+    pub file: bool,
+    pub line: bool,
+}
+
+impl Default for LogMetadataCtrlsConfig {
+    fn default() -> Self {
+        Self {
+            level: true,
+            datetime: true,
+            target: true,
+            file: false,
+            line: false,
+        }
+    }
+}
+
 pub struct InnerLogWin {
     main: MutPtr<QFrame>,
     table_view: MutPtr<QTableView>,
     view_ctrls: MutPtr<QFrame>,
     clear_button: MutPtr<QPushButton>,
+    trace_cb: MutPtr<QCheckBox>,
+    debug_cb: MutPtr<QCheckBox>,
+    info_cb: MutPtr<QCheckBox>,
+    warn_cb: MutPtr<QCheckBox>,
+    error_cb: MutPtr<QCheckBox>,
+    level_md_cb: MutPtr<QCheckBox>,
+    datetime_md_cb: MutPtr<QCheckBox>,
+    target_md_cb: MutPtr<QCheckBox>,
+    file_md_cb: MutPtr<QCheckBox>,
+    line_md_cb: MutPtr<QCheckBox>,
     model: MutPtr<QStandardItemModel>,
 }
 
 impl InnerLogWin {
-    pub unsafe fn new(parent: impl CastInto<MutPtr<QWidget>>) -> Self {
+    pub unsafe fn new(
+        parent: impl CastInto<MutPtr<QWidget>>,
+        levelconfig: &LogLevelCtrlsConfig,
+        metadataconfig: &LogMetadataCtrlsConfig,
+    ) -> Self {
         let mut main_frame = QFrame::new_0a();
         let main_frame_ptr = main_frame.as_mut_ptr();
         main_frame.set_object_name(&qs("LoggerMainFrame"));
@@ -97,22 +156,20 @@ impl InnerLogWin {
 
         // add the view to the main layout
         main_layout.add_widget(view.into_ptr());
-
-        // create view controls
-        let mut view_ctrls = QFrame::new_0a();
-        let view_ctrls_ptr = view_ctrls.as_mut_ptr();
-        let mut ctrls_layout = create_vlayout();
-
-        // add clear button
-        let mut clear_button = QPushButton::from_q_string(&QString::from_std_str("Clear"));
-        let clear_button_ptr = clear_button.as_mut_ptr();
-        ctrls_layout.add_widget(clear_button.into_ptr());
-
-        let label = QLabel::from_q_string(&qs("Controls"));
-        ctrls_layout.add_widget(label.into_ptr());
-        view_ctrls.set_layout(ctrls_layout.into_ptr());
-        main_layout.add_widget(view_ctrls.into_ptr());
-
+        let AddCtrlsReturn {
+            view_ctrls,
+            clear_button,
+            trace_cb,
+            debug_cb,
+            info_cb,
+            warn_cb,
+            error_cb,
+            level_md_cb,
+            datetime_md_cb,
+            target_md_cb,
+            file_md_cb,
+            line_md_cb,
+        } = Self::add_ctrls(main_layout.as_mut_ptr(), levelconfig, metadataconfig);
         // set the main layout
         main_frame.set_layout(main_layout.into_ptr());
 
@@ -124,9 +181,137 @@ impl InnerLogWin {
         Self {
             main: main_frame_ptr,
             table_view: view_ptr,
-            view_ctrls: view_ctrls_ptr,
-            clear_button: clear_button_ptr,
+            view_ctrls,
+            clear_button,
+            trace_cb,
+            debug_cb,
+            info_cb,
+            warn_cb,
+            error_cb,
+            level_md_cb,
+            datetime_md_cb,
+            target_md_cb,
+            file_md_cb,
+            line_md_cb,
             model: model_ptr,
+        }
+    }
+
+    fn add_ctrls(
+        mut layout: MutPtr<QHBoxLayout>,
+        levelconfig: &LogLevelCtrlsConfig,
+        metadataconfig: &LogMetadataCtrlsConfig,
+    ) -> AddCtrlsReturn {
+        unsafe {
+            // create view controls
+            let mut view_ctrls = QFrame::new_0a();
+            let view_ctrls_ptr = view_ctrls.as_mut_ptr();
+            let mut ctrls_layout = create_vlayout();
+
+            // add clear button
+            let mut clear_button = QPushButton::from_q_string(&QString::from_std_str("Clear"));
+            let clear_button_ptr = clear_button.as_mut_ptr();
+            ctrls_layout.add_widget(clear_button.into_ptr());
+
+            let mut loglevel_grp_box = QGroupBox::new();
+            let mut loglevel_grp_box_ptr = loglevel_grp_box.as_mut_ptr();
+            ctrls_layout.add_widget(loglevel_grp_box.into_ptr());
+            let name = qs("Active Log Levels");
+            loglevel_grp_box_ptr.set_title(&name);
+            loglevel_grp_box_ptr.set_object_name(&name);
+            let mut loglevel_grp_box_layout = create_vlayout();
+
+            let mut trace_cb = QCheckBox::from_q_string(&qs("Trace"));
+            let mut trace_cb_ptr = trace_cb.as_mut_ptr();
+            trace_cb_ptr.set_checked(levelconfig.trace);
+            loglevel_grp_box_layout.add_widget(trace_cb.into_ptr());
+
+            let mut debug_cb = QCheckBox::from_q_string(&qs("Debug"));
+            let mut debug_cb_ptr = debug_cb.as_mut_ptr();
+            debug_cb_ptr.set_checked(levelconfig.debug);
+
+            loglevel_grp_box_layout.add_widget(debug_cb.into_ptr());
+
+            let mut info_cb = QCheckBox::from_q_string(&qs("Info"));
+            let mut info_cb_ptr = info_cb.as_mut_ptr();
+            info_cb_ptr.set_checked(levelconfig.info);
+
+            loglevel_grp_box_layout.add_widget(info_cb.into_ptr());
+
+            let mut warn_cb = QCheckBox::from_q_string(&qs("Warn"));
+            let mut warn_cb_ptr = warn_cb.as_mut_ptr();
+            warn_cb_ptr.set_checked(levelconfig.warn);
+
+            loglevel_grp_box_layout.add_widget(warn_cb.into_ptr());
+
+            let mut error_cb = QCheckBox::from_q_string(&qs("Error"));
+            let mut error_cb_ptr = error_cb.as_mut_ptr();
+            error_cb_ptr.set_checked(levelconfig.error);
+
+            loglevel_grp_box_layout.add_widget(error_cb.into_ptr());
+
+            loglevel_grp_box_ptr.set_layout(loglevel_grp_box_layout.into_ptr());
+
+            // second group
+
+            let mut metadata_grp_box = QGroupBox::new();
+            let mut metadata_grp_box_ptr = metadata_grp_box.as_mut_ptr();
+            ctrls_layout.add_widget(metadata_grp_box.into_ptr());
+            let name = qs("Log Metadata");
+
+            metadata_grp_box_ptr.set_title(&name);
+            metadata_grp_box_ptr.set_object_name(&name);
+            let mut metadata_grp_box_layout = create_vlayout();
+
+            let mut level_cb = QCheckBox::from_q_string(&qs("Level"));
+            let mut level_cb_ptr = level_cb.as_mut_ptr();
+            level_cb_ptr.set_checked(metadataconfig.level);
+
+            metadata_grp_box_layout.add_widget(level_cb.into_ptr());
+
+            let mut datetime_cb = QCheckBox::from_q_string(&qs("DateTime"));
+            let mut datetime_cb_ptr = datetime_cb.as_mut_ptr();
+            datetime_cb_ptr.set_checked(metadataconfig.datetime);
+
+            metadata_grp_box_layout.add_widget(datetime_cb.into_ptr());
+
+            let mut target_cb = QCheckBox::from_q_string(&qs("Target"));
+            let mut target_cb_ptr = target_cb.as_mut_ptr();
+            target_cb_ptr.set_checked(metadataconfig.target);
+
+            metadata_grp_box_layout.add_widget(target_cb.into_ptr());
+
+            let mut file_cb = QCheckBox::from_q_string(&qs("File"));
+            let mut file_cb_ptr = file_cb.as_mut_ptr();
+            file_cb_ptr.set_checked(metadataconfig.file);
+
+            metadata_grp_box_layout.add_widget(file_cb.into_ptr());
+
+            let mut line_cb = QCheckBox::from_q_string(&qs("Line"));
+            let mut line_cb_ptr = line_cb.as_mut_ptr();
+            line_cb_ptr.set_checked(metadataconfig.line);
+
+            metadata_grp_box_layout.add_widget(line_cb.into_ptr());
+
+            metadata_grp_box_ptr.set_layout(metadata_grp_box_layout.into_ptr());
+
+            ctrls_layout.add_stretch_0a();
+            view_ctrls.set_layout(ctrls_layout.into_ptr());
+            layout.add_widget(view_ctrls.into_ptr());
+            AddCtrlsReturn {
+                view_ctrls: view_ctrls_ptr,
+                clear_button: clear_button_ptr,
+                trace_cb: trace_cb_ptr,
+                debug_cb: debug_cb_ptr,
+                info_cb: info_cb_ptr,
+                warn_cb: warn_cb_ptr,
+                error_cb: error_cb_ptr,
+                level_md_cb: level_cb_ptr,
+                datetime_md_cb: datetime_cb_ptr,
+                target_md_cb: target_cb_ptr,
+                file_md_cb: file_cb_ptr,
+                line_md_cb: line_cb_ptr,
+            }
         }
     }
 
@@ -144,15 +329,119 @@ impl InnerLogWin {
     pub fn ctrls(&self) -> MutPtr<QFrame> {
         self.view_ctrls
     }
+
     /// Return a mutable pointer to the clear button
     pub fn clear_button(&self) -> MutPtr<QPushButton> {
         self.clear_button
     }
+
     /// Retrieve a mutable pointer to the model
     pub fn model(&self) -> MutPtr<QStandardItemModel> {
         self.model
     }
 
+    /// Return the trace level checkbox from the log controlx
+    pub fn trace_cb(&self) -> MutPtr<QCheckBox> {
+        self.trace_cb
+    }
+
+    /// Return the debug level checkbox from the log controlx
+    pub fn debug_cb(&self) -> MutPtr<QCheckBox> {
+        self.debug_cb
+    }
+
+    /// Return the info level checkbox from the log controlx
+    pub fn info_cb(&self) -> MutPtr<QCheckBox> {
+        self.info_cb
+    }
+
+    /// Return the warn level checkbox from the log controlx
+    pub fn warn_cb(&self) -> MutPtr<QCheckBox> {
+        self.warn_cb
+    }
+
+    /// Return the error level checkbox from the log controlx
+    pub fn error_cb(&self) -> MutPtr<QCheckBox> {
+        self.error_cb
+    }
+
+    /// Return the level metadata checkbox from the log controlx
+    pub fn level_md_cb(&self) -> MutPtr<QCheckBox> {
+        self.level_md_cb
+    }
+    /// hide or unhide the level metadata checkbox
+    pub fn hide_level_md_cb(&self, hide: bool) {
+        unsafe {
+            let mut view = self.table_view();
+            view.set_column_hidden(0, hide);
+        }
+    }
+
+    /// Return the level metadata checkbox from the log controlx
+    pub fn datetime_md_cb(&self) -> MutPtr<QCheckBox> {
+        self.datetime_md_cb
+    }
+    /// hide or unhide the level metadata checkbox
+    pub fn hide_datetime_md_cb(&self, hide: bool) {
+        unsafe {
+            let mut view = self.table_view();
+            view.set_column_hidden(1, hide);
+        }
+    }
+
+    /// Return the target metadata checkbox from the log controlx
+    pub fn target_md_cb(&self) -> MutPtr<QCheckBox> {
+        self.target_md_cb
+    }
+
+    /// hide or unhide the target metadata checkbox
+    pub fn hide_target_md_cb(&self, hide: bool) {
+        unsafe {
+            let mut view = self.table_view();
+            view.set_column_hidden(2, hide);
+        }
+    }
+
+    /// Return the file metadata checkbox from the log controlx
+    pub fn file_md_cb(&self) -> MutPtr<QCheckBox> {
+        self.file_md_cb
+    }
+
+    /// hide or unhide the file metadata checkbox
+    pub fn hide_file_md_cb(&self, hide: bool) {
+        unsafe {
+            let mut view = self.table_view();
+            view.set_column_hidden(3, hide);
+        }
+    }
+
+    /// Return the line metadata checkbox from the log controlx
+    pub fn line_md_cb(&self) -> MutPtr<QCheckBox> {
+        self.line_md_cb
+    }
+
+    /// hide or unhide the line metadata checkbox
+    pub fn hide_line_md_cb(&self, hide: bool) {
+        unsafe {
+            let mut view = self.table_view();
+            view.set_column_hidden(4, hide);
+        }
+    }
+
+    /// Set the visibility of metadata columns based on the configuration struct
+    ///
+    /// # Arguments
+    /// * `config` - A reference to an instance of the LogMetadataCtrlsConfig struct
+    ///
+    /// # Returns
+    /// * None
+    pub fn configure_view_columns(&self, config: &LogMetadataCtrlsConfig) {
+        self.hide_level_md_cb(!config.level);
+        self.hide_datetime_md_cb(!config.datetime);
+        self.hide_target_md_cb(!config.target);
+        self.hide_file_md_cb(!config.file);
+        self.hide_line_md_cb(!config.line);
+    }
     /// perminantly clear the contents of the log
     pub fn clear_log(&self) {
         unsafe {
@@ -174,6 +463,7 @@ impl InnerLogWin {
         set_stylesheet_from_str(STYLE_STR, self.main);
     }
 
+    /// Log data
     pub fn log(&self, log_data: Option<LogData>, msg: &str) {
         unsafe {
             let mut item = QStandardItem::new();
@@ -398,4 +688,21 @@ impl InnerLogWin {
         model.set_item_3a(rc, 0, loglevel.into_ptr());
         model.set_item_3a(rc, 1, item.into_ptr());
     }
+}
+
+/// Structure returned by the add_ctrls associated
+/// function
+pub struct AddCtrlsReturn {
+    view_ctrls: MutPtr<QFrame>,
+    clear_button: MutPtr<QPushButton>,
+    trace_cb: MutPtr<QCheckBox>,
+    debug_cb: MutPtr<QCheckBox>,
+    info_cb: MutPtr<QCheckBox>,
+    warn_cb: MutPtr<QCheckBox>,
+    error_cb: MutPtr<QCheckBox>,
+    level_md_cb: MutPtr<QCheckBox>,
+    datetime_md_cb: MutPtr<QCheckBox>,
+    target_md_cb: MutPtr<QCheckBox>,
+    file_md_cb: MutPtr<QCheckBox>,
+    line_md_cb: MutPtr<QCheckBox>,
 }
--- pbgui-logger/src/log_win.rs
+++ pbgui-logger/src/log_win.rs
@@ -1,6 +1,6 @@
-use crate::inner_log_win::LogData;
+use crate::inner_log_win::{LogData, LogLevelCtrlsConfig, LogMetadataCtrlsConfig};
 use crate::InnerLogWin;
-use qt_core::Slot;
+use qt_core::{Slot, SlotOfInt};
 use qt_gui::QStandardItemModel;
 use qt_widgets::{
     cpp_core::{CastInto, MutPtr},
@@ -12,26 +12,76 @@ use std::rc::Rc;
 pub struct LogWin<'a> {
     inner_log_win: Rc<InnerLogWin>,
     clear_log: Slot<'a>,
+    toggle_level_cb: SlotOfInt<'a>,
+    toggle_datetime_cb: SlotOfInt<'a>,
+    toggle_target_cb: SlotOfInt<'a>,
+    toggle_file_cb: SlotOfInt<'a>,
+    toggle_line_cb: SlotOfInt<'a>,
 }
 
 impl<'a> LogWin<'a> {
     /// New up a LogWin instance
     pub unsafe fn new(parent: impl CastInto<MutPtr<QWidget>>) -> Self {
-        let inner = Rc::new(InnerLogWin::new(parent));
+        let log_level_ctrls_config = LogLevelCtrlsConfig::default();
+        let log_metadata_ctrls_config = LogMetadataCtrlsConfig::default();
+
+        let inner = Rc::new(InnerLogWin::new(
+            parent,
+            &log_level_ctrls_config,
+            &log_metadata_ctrls_config,
+        ));
         inner.set_default_stylesheet();
         let log_win = Self {
             inner_log_win: inner.clone(),
             clear_log: Slot::new(enclose! { (inner) move || {
                inner.clear_log();
             }}),
+            toggle_level_cb: SlotOfInt::new(enclose! {(inner) move |checked: i32| {
+                inner.hide_level_md_cb(checked<1)
+            }}),
+            toggle_datetime_cb: SlotOfInt::new(enclose! {(inner) move |checked: i32| {
+                inner.hide_datetime_md_cb(checked<1)
+            }}),
+            toggle_target_cb: SlotOfInt::new(enclose! {(inner) move |checked: i32| {
+                inner.hide_target_md_cb(checked<1)
+            }}),
+            toggle_file_cb: SlotOfInt::new(enclose! {(inner) move |checked: i32| {
+                inner.hide_file_md_cb(checked<1)
+            }}),
+            toggle_line_cb: SlotOfInt::new(enclose! {(inner) move |checked: i32| {
+                inner.hide_line_md_cb(checked<1)
+            }}),
         };
+
         log_win
             .inner()
             .clear_button()
             .clicked()
             .connect(&log_win.clear_log);
         //configure
-        log_win.inner().set_ctrls_visible(false);
+        let inner = log_win.inner();
+        inner.set_ctrls_visible(false);
+        inner.configure_view_columns(&log_metadata_ctrls_config);
+        inner
+            .level_md_cb()
+            .state_changed()
+            .connect(&log_win.toggle_level_cb);
+        inner
+            .datetime_md_cb()
+            .state_changed()
+            .connect(&log_win.toggle_datetime_cb);
+        inner
+            .target_md_cb()
+            .state_changed()
+            .connect(&log_win.toggle_target_cb);
+        inner
+            .file_md_cb()
+            .state_changed()
+            .connect(&log_win.toggle_file_cb);
+        inner
+            .line_md_cb()
+            .state_changed()
+            .connect(&log_win.toggle_line_cb);
         log_win
     }
     /// Retrieve the reference counted pointer to the InnerLogWin
