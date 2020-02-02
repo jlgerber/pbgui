9bd3540650a09578a0c11c6bb2a0232090eeb91e
jlgerber <jlgerber@gmail.com>
2020-02-02 15:08:26 -0800

    generates package.xml from components

--- pbgui/src/io/packages.rs
+++ pbgui/src/io/packages.rs
@@ -5,8 +5,20 @@
 //use serde::{Deserialize, Serialize};
 use simple_xml_serialize::XMLElement;
 use simple_xml_serialize_macro::xml_element;
+use std::mem;
 
-//#[derive(Debug, Serialize, Deserialize, PartialEq)]
+/// Determine if an XML element is closed (eg <foo />)
+pub trait IsClosed {
+    fn is_closed(&self) -> bool;
+}
+
+impl IsClosed for XMLElement {
+    fn is_closed(&self) -> bool {
+        self.contents.is_none() && self.text.is_none()
+    }
+}
+
+/// Top level element
 #[xml_element("show")]
 pub struct Show {
     #[sxs_type_attr]
@@ -60,16 +72,57 @@ pub struct Package {
     name: String,
     #[sxs_type_attr]
     version: String,
+    #[sxs_type_attr]
+    os: Option<String>,
+    #[sxs_type_attr]
+    site: Option<String>,
     #[sxs_type_multi_element(rename = "withs")]
     withs: Vec<With>,
 }
+
 impl Package {
     /// New up a package
-    pub fn new<I: Into<String>>(name: I, version: I) -> Self {
+    pub fn new<I: Into<String>>(name: I, version: I, os: Option<I>, site: Option<I>) -> Self {
         Self {
             name: name.into(),
             version: version.into(),
             withs: Vec::new(),
+            os: os.map(|x| x.into()),
+            site: site.map(|x| x.into()),
+        }
+    }
+
+    /// New up a Package instance given the name and version of a distribution
+    pub fn from_name_and_version<I: Into<String>>(name: I, version: I) -> Self {
+        Self {
+            name: name.into(),
+            version: version.into(),
+            withs: Vec::new(),
+            os: None,
+            site: None,
+        }
+    }
+
+    /// Construct a Package from a distribution, returning an Option
+    ///
+    /// # Arguments
+    ///
+    /// * `dist` - The distribution, eg foo-1.2.3
+    ///
+    /// # Returns
+    /// * Some(package) if successful
+    /// * None otherwise
+    pub fn from_dist(dist: &str) -> Option<Self> {
+        if let &[name, version] = &*dist.split("-").collect::<Vec<_>>() {
+            Some(Self {
+                name: name.into(),
+                version: version.into(),
+                withs: Vec::new(),
+                os: None,
+                site: None,
+            })
+        } else {
+            None
         }
     }
 
@@ -78,8 +131,52 @@ impl Package {
         self.withs.push(with);
     }
 
+    /// Add a with package, returning Self. Used as part of a builder pattern.
     pub fn add_with_owned(mut self, with: With) -> Self {
         self.withs.push(with);
+        self
+    }
+
+    /// Set the os field.
+    pub fn set_os<I: Into<String>>(&mut self, os: Option<I>) {
+        self.os = os.map(|x| x.into());
+    }
+    /// Set the os field and return Self. Used as part of a builder pattern
+    ///
+    /// # Arguments
+    ///
+    /// * `os` - An Option wrapped platform name
+    ///
+    /// # Returns
+    /// * Self
+    pub fn set_os_owned<I: Into<String>>(mut self, os: Option<I>) -> Self {
+        self.os = os.map(|x| x.into());
+        self
+    }
+
+    /// Set the site.
+    ///
+    /// # Arguments
+    ///
+    /// * `site` - An Option wrapped site name
+    ///
+    /// Returns
+    /// None
+    pub fn set_site<I: Into<String>>(&mut self, site: Option<I>) {
+        self.site = site.map(|x| x.into());
+    }
+
+    /// Set the site and return Self. Used as part of a builder pattern
+    ///
+    /// # Arguments
+    ///
+    /// * `site` - An Option wrapped site name
+    ///
+    /// # Returns
+    ///
+    /// * Self
+    pub fn set_site_owned<I: Into<String>>(mut self, site: Option<I>) -> Self {
+        self.site = site.map(|x| x.into());
         self
     }
 }
@@ -108,7 +205,11 @@ impl Roles {
     pub fn new() -> Roles {
         Roles { role: Vec::new() }
     }
-
+    /// Add a role into the list of child roles
+    ///
+    /// # Arguments
+    ///
+    /// * `role` - An instance of type Role
     pub fn push(&mut self, role: Role) {
         self.role.push(role)
     }
@@ -133,13 +234,81 @@ impl Role {
         self.packages.push(package);
     }
 
-    /// add a package to the packages managed by the role
+    /// add a package to the packages managed by the role, returning Self. Used
+    /// as part of a builder pattern.
+    ///
+    /// # Parameters
+    ///
+    /// * `package` - An instance of Package
     pub fn add_package_owned(mut self, package: Package) -> Self {
         self.packages.push(package);
         self
     }
 }
 
+/// Converter which generates an XMLElement Tree from an entry which impls  Into<XMLElement>
+///
+/// # Example
+///
+/// ```rust
+/// use simple_xml_serialize::XMLElement;
+///
+/// fn main() {
+///      let mut show = Show::new("FACILITY");
+///
+///      show.add_package(
+///      Package::new("maya", "2018.5.1", None, None)
+///         .set_os_owned(Some("cent7_64"))
+///         .add_with_owned(With::new("xerces"))
+///         .add_with_owned(With::new("mayapipeline"))
+///      );
+///
+///     let xml = ToXml::new().to_xml(show);
+/// }
+pub struct ToXml {
+    pub prune_closed: bool,
+}
+
+impl ToXml {
+    pub fn new() -> Self {
+        Self { prune_closed: true }
+    }
+    /// Convert entry into xml element provided it implw Into<XMLElemet>
+    pub fn to_xml(&self, entry: impl Into<XMLElement>) -> XMLElement {
+        let mut xml = entry.into();
+        if self.prune_closed {
+            Self::prune_closed_contents(&mut xml);
+        }
+        xml
+    }
+
+    /// Given an element, generate a pretty string rep
+    pub fn to_pretty_string(elem: &XMLElement) -> String {
+        elem.to_string_pretty("\n", "  ")
+    }
+
+    /// Given an element, generate a string representation
+    pub fn to_string(elem: &XMLElement) -> String {
+        elem.to_string()
+    }
+
+    // given a node, prune its closed contents
+    fn prune_closed_contents(elem: &mut XMLElement) {
+        if elem.contents.is_some() {
+            let contents = elem.contents.take().unwrap();
+            let contents = contents
+                .into_iter()
+                .filter(|x| !x.is_closed())
+                .collect::<Vec<_>>();
+            let mut contents = if contents.len() > 0 {
+                Some(contents)
+            } else {
+                None
+            };
+            mem::swap(&mut elem.contents, &mut contents);
+        }
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -147,26 +316,29 @@ mod tests {
     fn can_serialize_show_no_role() {
         let mut show = Show::new("FACILITY");
         show.add_package(
-            Package::new("maya", "2018.5.1")
+            Package::new("maya", "2018.5.1", None, None)
+                .set_os_owned(Some("cent7_64"))
                 .add_with_owned(With::new("xerces"))
                 .add_with_owned(With::new("mayapipeline")),
         );
         show.add_package(
-            Package::new("houdini", "17.5.432")
+            Package::new("houdini", "17.5.432", None, None)
                 .add_with_owned(With::new("houd_pipeline"))
                 .add_with_owned(With::new("houd_camera")),
         );
-        let xml = XMLElement::from(show);
-        assert_eq!(xml.to_string_pretty("\n", "  ").as_str(), 
-        "<show name=\"FACILITY\">\n  <packages name=\"maya\" version=\"2018.5.1\">\n     <withs package=\"xerces\"/>\n    <withs package=\"mayapipeline\"/>\n  </packages>\n  <packages name=\"houdini\" version=\"17.5.432\">\n    <withs package=\"houd_pipeline\"/>\n    <withs package=\"houd_camera\"/>\n  </packages>\n</show>"
+        let converter = ToXml::new();
+        let xml = converter.to_xml(show);
+        assert_eq!(xml.to_string_pretty("\n", "  ").as_str(),
+        "<show name=\"FACILITY\">\n  <packages>\n    <package name=\"maya\" version=\"2018.5.1\" os=\"cent7_64\">\n      <withs package=\"xerces\"/>\n      <withs package=\"mayapipeline\"/>\n    </package>\n    <package name=\"houdini\" version=\"17.5.432\">\n      <withs package=\"houd_pipeline\"/>\n      <withs package=\"houd_camera\"/>\n    </package>\n  </packages>\n</show>"
         );
     }
     #[test]
     fn can_serialize_show_no_withs() {
         let mut show = Show::new("FACILITY");
-        show.add_package(Package::new("maya", "2018.5.1"));
-        show.add_package(Package::new("houdini", "17.5.432"));
-        let xml = XMLElement::from(show);
+        show.add_package(Package::new("maya", "2018.5.1", None, None));
+        show.add_package(Package::new("houdini", "17.5.432", None, None));
+        let converter = ToXml::new();
+        let xml = converter.to_xml(show);
         assert_eq!(xml.to_string_pretty("\n", "  ").as_str(), 
         "<show name=\"FACILITY\">\n  <packages name=\"maya\" version=\"2018.5.1\"/>\n  <packages name=\"houdini\" version=\"17.5.432\"/>\n</show>"
         );
@@ -176,12 +348,12 @@ mod tests {
     fn can_serialize_show_with_roles() {
         let mut show = Show::new("FACILITY");
         show.add_package(
-            Package::new("maya", "2018.5.1")
+            Package::new("maya", "2018.5.1", None, None)
                 .add_with_owned(With::new("xerces"))
                 .add_with_owned(With::new("mayapipeline")),
         );
         show.add_package(
-            Package::new("houdini", "17.5.432")
+            Package::new("houdini", "17.5.432", None, None)
                 .add_with_owned(With::new("houd_pipeline"))
                 .add_with_owned(With::new("houd_camera")),
         );
@@ -189,14 +361,14 @@ mod tests {
         show.add_role(
             Role::new("model")
                 .add_package_owned(
-                    Package::new("maya", "2020.1.0")
+                    Package::new("maya", "2020.1.0", None, None)
                         .add_with_owned(With::new("xerces"))
                         .add_with_owned(With::new("mayapipeline"))
                         .add_with_owned(With::new("modelpipeline")),
                 )
-                .add_package_owned(Package::new("zbrush", "14"))
+                .add_package_owned(Package::new("zbrush", "14", None, None))
                 .add_package_owned(
-                    Package::new("atomic", "1.2.3")
+                    Package::new("atomic", "1.2.3", None, None)
                         .add_with_owned(With::new("vray"))
                         .add_with_owned(With::new("vray_for_maya")),
                 ),
