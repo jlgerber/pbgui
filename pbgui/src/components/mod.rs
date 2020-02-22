776d5b65f0c8bed38df5a312051505795d4dec7d
jlgerber <jlgerber@gmail.com>
2020-02-22 08:41:42 -0800

    changes visibilty of components and slot functions from pub to pub(crate)

--- pbgui/src/components/mod.rs
+++ pbgui/src/components/mod.rs
@@ -4,22 +4,22 @@
 /// The bottom_context_widget provides the bottom stacked widget with a per
 /// stack set of controls that are hosted on the toolbar between the versionpin
 /// table and the revision view below it.
-pub mod bottom_context_widget;
+pub(crate) mod bottom_context_widget;
 /// The bottom stacked widget housese the change table , the hstory table
-pub mod bottom_stacked_widget;
+pub(crate) mod bottom_stacked_widget;
 
-pub mod center_widget;
-pub mod dist_tree;
-pub mod left_toolbar;
-pub mod package_withs_list;
-pub mod packages_tree;
+pub(crate) mod center_widget;
+pub(crate) mod dist_tree;
+pub(crate) mod left_toolbar;
+pub(crate) mod package_withs_list;
+pub(crate) mod packages_tree;
 /// Part of the history widget that shows changes for a particular revision
-pub mod revision_changes_table;
-pub mod revisions_table;
-pub mod table_headers;
-pub mod versionpin_changes_row;
-pub mod versionpin_changes_table;
-pub mod versionpin_row;
-pub mod versionpin_table;
-pub mod versionpin_table_splitter;
-pub mod withs_splitter;
+pub(crate) mod revision_changes_table;
+pub(crate) mod revisions_table;
+pub(crate) mod table_headers;
+pub(crate) mod versionpin_changes_row;
+pub(crate) mod versionpin_changes_table;
+pub(crate) mod versionpin_row;
+pub(crate) mod versionpin_table;
+pub(crate) mod versionpin_table_splitter;
+pub(crate) mod withs_splitter;
--- pbgui/src/lib.rs
+++ pbgui/src/lib.rs
@@ -5,12 +5,12 @@ pub mod utility;
 // needed so that qt wont segfault (what was I referring to????)
 pub mod components;
 pub use components::left_toolbar::LeftToolBarActions;
-pub use components::{
+pub(crate) use components::{
     bottom_stacked_widget, center_widget, left_toolbar, package_withs_list, packages_tree,
     revisions_table, table_headers, versionpin_changes_row, versionpin_changes_table,
     versionpin_row, versionpin_table, versionpin_table_splitter, withs_splitter,
 };
-pub use slot_functions::{
+pub(crate) use slot_functions::{
     choose_distribution, save_packages_xml, save_versionpin_changes, select_history,
     store_withpackage_changes, update_changes_table, update_versionpin_table, update_withpackages,
 };
