ef993f4ec861a3cecf796ca82a5670f0b6d44189
jlgerber <jlgerber@gmail.com>
2020-02-23 02:08:35 -0800

    removes commented out code from main_win.rs

--- pbgui/src/messaging/thread/main_win.rs
+++ pbgui/src/messaging/thread/main_win.rs
@@ -36,8 +36,6 @@ pub(crate) fn match_main_win(
                 .search_mode(LtreeSearchMode::from_str(dir.as_str()).expect("unable to find vpin"))
                 .query();
 
-            //let filter_package = if line_edit_txt != "" { true } else { false };
-
             let mut vpins = match results {
                 Ok(vpins) => vpins,
                 Err(err) => {
@@ -105,7 +103,6 @@ pub(crate) fn match_main_win(
         OMainWin::GetHistoryRevisions => {
             let results = db
                 .find_all_revisions()
-                //transaction_id(tx_id as i64)
                 .order_by(vec![OrderRevisionBy::Id])
                 .order_direction(OrderDirection::Desc)
                 .query();
@@ -132,25 +129,6 @@ pub(crate) fn match_main_win(
             comments,
             user,
         } => {
-            // let results = db
-            //     .find_all_revisions()
-            //     //transaction_id(tx_id as i64)
-            //     .order_by(vec![OrderRevisionBy::Id])
-            //     .order_direction(OrderDirection::Desc)
-            //     .query();
-            // let revisions = match results {
-            //     Ok(revisions) => revisions,
-            //     Err(err) => {
-            //         sender
-            //             .send(IMsg::Error(format!(
-            //                 "Unable to get with revisions from db: {}",
-            //                 err
-            //             )))
-            //             .expect("unable to send error msg");
-            //         conductor.signal(Event::Error);
-            //         return;
-            //     }
-            // };
             let mut tx = db.transaction();
             let mut tx_cnt = 0;
             for change in changes {
@@ -192,12 +170,9 @@ pub(crate) fn match_main_win(
                         add_versionpins = add_versionpins.role(role);
                         add_versionpins = add_versionpins.platform(platform);
 
-                        //let username = whoami::username();
-                        //let comment = "auto added change";
                         add_versionpins = match add_versionpins.create() {
                             Ok(add_versionpins) => add_versionpins,
                             Err(err) => {
-                                println!("in thread. unable to call add_versionpins.create()");
                                 sender
                                     .send(IMsg::Error(format!(
                                         "Unable to call add_versionpins.create(): {}",
@@ -208,37 +183,12 @@ pub(crate) fn match_main_win(
                                 return;
                             }
                         };
-                        println!("called add_versionpins.create()");
                         tx = add_versionpins.take_tx();
-                        // let update_cnt = match add_versionpins
-                        //     .create()
-                        //     .unwrap()
-                        //     .commit(&username, &comment)
-                        // {
-                        //     Ok(update_cnt) => update_cnt,
-                        //     Err(err) => {
-                        //         sender
-                        //             .send(IMsg::Error(format!(
-                        //                 "Unable to add versionpin to db: {}",
-                        //                 err
-                        //             )))
-                        //             .expect("unable to send error msg");
-                        //         conductor.signal(Event::Error);
-                        //         return;
-                        //     }
-                        // };
-
-                        //println!("called add_versionpins.commit");
-                        // not working
-                        // tx = add_versionpins.take_tx();
-                        //.expect("failed to call add_versionpins.commit");
-                        //tx_cnt += update_cnt;
                     }
                     _ => panic!("not implemented"),
                 }
             }
             let results = PackratDb::commit(tx, user.as_str(), comments.as_str(), tx_cnt);
-            println!("results are {:?}", results);
             sender
                 .send(IMainWin::SaveVpinChanges(results.is_ok()).to_imsg())
                 .expect("unable to send changes");
@@ -285,119 +235,6 @@ pub(crate) fn match_main_win(
                     conductor.signal(Event::Error);
                 }
             }
-            // // TODO: Impl
-            // // get a list of version pins for the show
-            // let results = db
-            //     .find_all_versionpins()
-            //     .isolate_facility(true)
-            //     .level(show.as_str())
-            //     .search_mode(LtreeSearchMode::Descendant)
-            //     .order_by(vec![SearchAttribute::Role, SearchAttribute::Package])
-            //     .query();
-            // let vpins = match results {
-            //     Ok(vpins) => vpins,
-            //     Err(err) => {
-            //         sender
-            //             .send(IMsg::Error(format!(
-            //                 "Unable to get version pins from db: {}",
-            //                 err
-            //             )))
-            //             .expect("unable to send error msg");
-            //         conductor.signal(Event::Error);
-            //         return;
-            //     }
-            // };
-            // // get a list of withs for the show
-            // // iterate through version pins, creating appropriate data structure for outgoing
-            // let mut show = io::Show::new(show);
-            // let mut last_role: Option<Role> = None;
-            // let mut role_packages = Vec::new();
-            // for row in vpins {
-            //     let package = row.distribution.package();
-            //     let version = row.distribution.version();
-            //     let site = row.coords.site();
-            //     let platform = row.coords.platform();
-            //     // TODO: do not know how seq/shot is handled in packages.xml
-            //     let level = row.coords.level(); // hwo is this handled?
-            //     let role = row.coords.role();
-            //     let mut package = io::Package::new(package, version, None, None);
-            //     if let Some(withs) = row.withs {
-            //         for with in withs {
-            //             package.add_with(io::With::new(with));
-            //         }
-            //     }
-            //     if site != &Site::Any {
-            //         package.set_site(Some(site.to_string()));
-            //     }
-            //     if platform != &Platform::Any {
-            //         package.set_os(Some(platform.to_string()));
-            //     }
-            //     if role != &Role::Any {
-            //         let role_str = role.to_string();
-
-            //         // if our last iter was a role
-            //         if let Some(ref last) = last_role {
-            //             // if the current role is the same as the last
-            //             // role, we add the package into our list
-            //             if role == last {
-            //                 role_packages.push(package);
-            //             } else {
-            //                 // otherwise we drain the list of saved packages,
-            //                 // adding them in to the current role
-            //                 let mut role_tag = io::Role::new(role_str);
-            //                 for pkg in role_packages.drain(..) {
-            //                     role_tag.add_package(pkg);
-            //                 }
-            //                 show.add_role(role_tag);
-            //                 // and we push the current package into our list,
-            //                 // which is now empty
-            //                 role_packages.push(package);
-            //             }
-            //         } else {
-            //             // in the case where our last iter was NOT a role
-            //             // role packages should be zero sized
-            //             assert_eq!(role_packages.len(), 0);
-            //         }
-            //         last_role = Some(role.clone());
-            //     } else {
-            //         show.add_package(package);
-            //         last_role = None;
-            //     }
-            // }
-            // // serialise to disk
-            // let xml_writer = io::ToXml::new();
-            // let show = xml_writer.to_xml(show);
-            // let xml_str = io::ToXml::to_pretty_string(&show);
-            // let mut output = match File::create(output) {
-            //     Ok(output) => output,
-            //     Err(err) => {
-            //         sender
-            //             .send(IMsg::Error(format!(
-            //                 "Unable to create packages.xml for writing: {}",
-            //                 err
-            //             )))
-            //             .expect("unable to send error msg");
-            //         conductor.signal(Event::Error);
-            //         return;
-            //     }
-            // };
-            // match write!(output, "{}", xml_str) {
-            //     Ok(_) => {
-            //         sender
-            //             .send(IMainWin::SavePackagesXml(true).to_imsg())
-            //             .expect("unable to send changes");
-            //         conductor.signal(MainWin::SavePackagesXml.to_event());
-            //     }
-            //     Err(err) => {
-            //         sender
-            //             .send(IMsg::Error(format!(
-            //                 "Unable to write packages.xml: {}",
-            //                 err
-            //             )))
-            //             .expect("unable to send error msg");
-            //         conductor.signal(Event::Error);
-            //     }
-            // };
         }
     }
 }
